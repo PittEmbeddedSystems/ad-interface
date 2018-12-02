b8c3525ee360ef4ebc586416cb89435ab781319a
Lindsey <marralindseyj@gmail.com>
2018-12-01 19:17:30 -0500

    Updates version number in preparation for release of version 0.2.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='ad-interface',
-      version='0.1',
+      version='0.2',
       description='library for reading A/D outputs',
       py_modules=['ad_interface', 'ad_mcp', 'ad_fake']
     )
