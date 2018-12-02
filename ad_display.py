b0494778a25577cd7069b6116cf59bb1ba9e7442
Lindsey <marralindseyj@gmail.com>
2018-12-01 19:15:02 -0500

    Fixes output in display function.

--- ad_display.py
+++ ad_display.py
@@ -10,6 +10,6 @@ def getData():
 while True:
     
     sensorData = [mcp.read_adc(0), mcp.read_adc(1), mcp.read_adc(2), mcp.read_adc(3), mcp.read_adc(4), mcp.read_adc(5), mcp.read_adc(6), mcp.read_adc(7)]
-    print(str(mcp.read_adc(0)) + " " + str(mcp.read_adc(1)) + " " + str(mcp.read_adc(2)) + " " + str(getData()))
+    print(str(getData()))
     time.sleep(1)
     
